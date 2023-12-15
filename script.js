5970506676647aa9fcd148a457eb544653fc4198
´´pedro <pedrohbfirmes@gmail.com>
2023-12-14 22:08:36 -0300

    :tada:initial commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-light.png")
-  } else {
+  } else 
     // set tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
   }
