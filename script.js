1000bad8a4b3645893474dd6dba3d4ae2acc22e3
´´pedro <pedrohbfirmes@gmail.com>
2023-12-14 22:37:03 -0300

    add readme

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ function toggleMode() {
     img.setAttribute("src", "./assets/Maracanã.jpg")
   } else {
     // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/Avatar.png")
   }
 }
